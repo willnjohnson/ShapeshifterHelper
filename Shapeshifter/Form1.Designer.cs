5e75f716777d47e42bf643bc2c20c0cfb17e098e
William Johnson <wnj@tutamail.com>
2025-07-11 16:43:53 -0400

    Add project files.

---
+++ Shapeshifter/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Shapeshifter
+{
+    partial class ShapeShifter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stepsPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.headerPaste = new System.Windows.Forms.Label();
+            this.startStopButton = new System.Windows.Forms.Button();
+            this.headerResult = new System.Windows.Forms.Label();
+            this.waitingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // stepsPanel
+            // 
+            this.stepsPanel.AutoScroll = true;
+            this.stepsPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.stepsPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.stepsPanel.ForeColor = System.Drawing.Color.White;
+            this.stepsPanel.Location = new System.Drawing.Point(16, 493);
+            this.stepsPanel.Name = "stepsPanel";
+            this.stepsPanel.Size = new System.Drawing.Size(772, 233);
+            this.stepsPanel.TabIndex = 5;
+            this.stepsPanel.WrapContents = false;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.BackColor = System.Drawing.Color.DimGray;
+            this.inputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.inputTextBox.ForeColor = System.Drawing.Color.White;
+            this.inputTextBox.Location = new System.Drawing.Point(16, 51);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputTextBox.Size = new System.Drawing.Size(772, 328);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // headerPaste
+            // 
+            this.headerPaste.AutoSize = true;
+            this.headerPaste.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.headerPaste.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.headerPaste.Location = new System.Drawing.Point(12, 28);
+            this.headerPaste.Name = "headerPaste";
+            this.headerPaste.Size = new System.Drawing.Size(238, 20);
+            this.headerPaste.TabIndex = 1;
+            this.headerPaste.Text = "Paste ShapeShifter HTML code:";
+            this.headerPaste.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // startStopButton
+            // 
+            this.startStopButton.BackColor = System.Drawing.Color.DimGray;
+            this.startStopButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.startStopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.startStopButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.startStopButton.Location = new System.Drawing.Point(16, 385);
+            this.startStopButton.Name = "startStopButton";
+            this.startStopButton.Size = new System.Drawing.Size(109, 48);
+            this.startStopButton.TabIndex = 2;
+            this.startStopButton.Text = "Start";
+            this.startStopButton.UseVisualStyleBackColor = false;
+            this.startStopButton.Click += new System.EventHandler(this.startStopButton_Click);
+            // 
+            // headerResult
+            // 
+            this.headerResult.AutoSize = true;
+            this.headerResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.headerResult.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.headerResult.Location = new System.Drawing.Point(12, 470);
+            this.headerResult.Name = "headerResult";
+            this.headerResult.Size = new System.Drawing.Size(59, 20);
+            this.headerResult.TabIndex = 4;
+            this.headerResult.Text = "Result:";
+            this.headerResult.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // waitingLabel
+            // 
+            this.waitingLabel.AutoSize = true;
+            this.waitingLabel.ForeColor = System.Drawing.Color.Transparent;
+            this.waitingLabel.Location = new System.Drawing.Point(131, 404);
+            this.waitingLabel.Name = "waitingLabel";
+            this.waitingLabel.Size = new System.Drawing.Size(71, 13);
+            this.waitingLabel.TabIndex = 6;
+            this.waitingLabel.Text = "Calculating ...";
+            this.waitingLabel.Visible = false;
+            this.waitingLabel.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // ShapeShifter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 738);
+            this.Controls.Add(this.waitingLabel);
+            this.Controls.Add(this.stepsPanel);
+            this.Controls.Add(this.headerResult);
+            this.Controls.Add(this.startStopButton);
+            this.Controls.Add(this.headerPaste);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "ShapeShifter";
+            this.ShowIcon = false;
+            this.Text = "ShapeShifter Solver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label headerPaste;
+        private System.Windows.Forms.Button startStopButton;
+        private System.Windows.Forms.Label headerResult;
+        private System.Windows.Forms.Label waitingLabel;
+        private System.Windows.Forms.FlowLayoutPanel stepsPanel;
+    }
+}
+
---
+++ Shapeshifter/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shapeshifter
+{
+    public partial class ShapeShifter : Form
+    {
+        private string ParseGridLayout(string html)
+        {
+            var regex = new Regex(@"mouseon\((\d+),(\d+)\)""[^>]*>\s*<img[^>]+/([^/_]+)_\d+\.gif", RegexOptions.IgnoreCase);
+
+            int maxCol = -1;
+            int maxRow = -1;
+            var tileMap = new Dictionary<(int, int), string>();
+
+            foreach (Match match in regex.Matches(html))
+            {
+                int x = int.Parse(match.Groups[1].Value);
+                int y = int.Parse(match.Groups[2].Value);
+                string tile = match.Groups[3].Value.ToLower();
+
+                if (x > maxCol) maxCol = x;
+                if (y > maxRow) maxRow = y;
+
+                tileMap[(x, y)] = tile == "swo" ? "0" : "1";
+            }
+
+            int width = maxCol + 1;
+            int height = maxRow + 1;
+            var grid = new string[height, width];
+
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    grid[y, x] = tileMap.TryGetValue((x, y), out var value) ? value : "0";
+
+            var layoutRows = new List<string>();
+            for (int y = 0; y < height; y++)
+            {
+                var sb = new StringBuilder();
+                for (int x = 0; x < width; x++)
+                    sb.Append(grid[y, x]);
+                layoutRows.Add(sb.ToString());
+            }
+
+            return string.Join(",", layoutRows);
+        }
+
+        private List<string> ExtractTokenLayouts(string html)
+        {
+            var tokenList = new List<string>();
+
+            var tableMatcher = new Regex(@"<table.*?>(.*?)<\/table>", RegexOptions.Singleline);
+            var rowMatcher = new Regex(@"<tr>(.*?)<\/tr>", RegexOptions.Singleline);
+            var fillDetector = new Regex(@"<img[^>]+square\.gif", RegexOptions.IgnoreCase);
+
+            foreach (Match tableMatch in tableMatcher.Matches(html))
+            {
+                var content = tableMatch.Groups[1].Value;
+                var rowSpecs = new List<string>();
+
+                foreach (Match rowMatch in rowMatcher.Matches(content))
+                {
+                    var rowContent = rowMatch.Groups[1].Value;
+                    var cells = rowContent.Split(new[] { "</td>" }, StringSplitOptions.None);
+                    var sb = new StringBuilder();
+
+                    foreach (var cellHtml in cells)
+                    {
+                        bool filled = fillDetector.IsMatch(cellHtml);
+                        sb.Append(filled ? "1" : "0");
+                    }
+
+                    rowSpecs.Add(sb.ToString());
+                }
+
+                int maxActiveCol = 0;
+                for (int col = 0; ; col++)
+                {
+                    bool found = false;
+                    foreach (var row in rowSpecs)
+                    {
+                        if (col < row.Length && row[col] == '1')
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) break;
+                    maxActiveCol = col;
+                }
+
+                var cleanedRows = rowSpecs
+                    .Select(r => r.Substring(0, Math.Min(r.Length, maxActiveCol + 1)))
+                    .Where(r => r.Contains('1'))
+                    .ToList();
+
+                if (cleanedRows.Count > 0)
+                {
+                    tokenList.Add(string.Join(",", cleanedRows));
+                }
+            }
+
+            return tokenList;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void label1_Click_1(object sender, EventArgs e) { }
+
+        private void label1_Click_2(object sender, EventArgs e) { }
+
+        public ShapeShifter()
+        {
+            InitializeComponent();
+        }
+
+        private async void startStopButton_Click(object sender, EventArgs e)
+        {
+            waitingLabel.Visible = true;      // Show waiting message
+            startStopButton.Enabled = false; // Disable button
+
+            // We'll capture inputTextBox.Text outside Task.Run for thread safety
+            string htmlContent = inputTextBox.Text;
+
+            bool solved = false;
+            List<(int X, int Y)> placements = null;
+            List<ShapeToken> tokenObjects = null;
+
+            await Task.Run(() =>
+            {
+                string gridPattern = ParseGridLayout(htmlContent);
+                var tokenPatterns = ExtractTokenLayouts(htmlContent);
+                tokenObjects = tokenPatterns.Select(p => new ShapeToken(p)).ToList();
+
+                var puzzle = new TileGrid(gridPattern);
+                var planner = new ShapeShifterSolver(puzzle, tokenObjects);
+                solved = planner.AttemptSolve();
+
+                if (solved)
+                {
+                    var placementDict = planner.GetPlacements();
+                    placements = new List<(int X, int Y)>(tokenObjects.Count);
+                    for (int i = 0; i < tokenObjects.Count; i++)
+                    {
+                        if (placementDict.TryGetValue(i, out var pos))
+                            placements.Add(pos);
+                        else
+                            placements.Add((-1, -1));
+                    }
+                }
+            });
+
+            // Now update UI on the main thread:
+            stepsPanel.Controls.Clear();
+
+            if (solved)
+            {
+                for (int i = 0; i < tokenObjects.Count; i++)
+                {
+                    var pos = placements[i];
+                    var checkbox = new CheckBox
+                    {
+                        Text = $"Step {i + 1}: Row {pos.Y}, Column {pos.X}",
+                        AutoSize = true
+                    };
+
+                    checkbox.CheckedChanged += (s, eArgs) => HighlightNextStep();
+
+                    var wrapper = new Panel
+                    {
+                        BorderStyle = BorderStyle.None,
+                        AutoSize = true,
+                        Padding = new Padding(3),
+                        Margin = new Padding(5)
+                    };
+
+                    wrapper.Controls.Add(checkbox);
+                    stepsPanel.Controls.Add(wrapper);
+                }
+            }
+            else
+            {
+                var label = new Label
+                {
+                    Text = "No arrangement found.",
+                    AutoSize = true,
+                    ForeColor = Color.Red,
+                    Font = new Font("Segoe UI", 10, FontStyle.Bold),
+                    Padding = new Padding(10)
+                };
+                stepsPanel.Controls.Add(label);
+            }
+
+            HighlightNextStep();
+
+            waitingLabel.Visible = false;     // Hide waiting message
+            startStopButton.Enabled = true;  // Re-enable button
+        }
+
+        private void HighlightNextStep()
+        {
+            foreach (Control wrapper in stepsPanel.Controls)
+            {
+                if (wrapper is Panel panel && panel.Controls[0] is CheckBox cb)
+                {
+                    if (!cb.Checked)
+                    {
+                        panel.BorderStyle = BorderStyle.FixedSingle;
+                        cb.Focus();
+                        break;
+                    }
+                    else
+                    {
+                        panel.BorderStyle = BorderStyle.None;
+                    }
+                }
+            }
+        }
+    }
+
+    public class TileGrid
+    {
+        private int[] tiles;
+        public int Rows { get; }
+        public int Cols { get; }
+        public int Wrap { get; }
+
+        public TileGrid(string data, int wrap = 1)
+        {
+            var rowData = data.Split(',');
+            Rows = rowData.Length;
+            Cols = rowData[0].Length;
+            Wrap = wrap;
+
+            tiles = new int[Rows * Cols];
+            int i = 0;
+            foreach (var row in rowData)
+                foreach (var c in row)
+                    tiles[i++] = c == '1' ? 1 : 0;
+        }
+
+        public void Mark(ShapeToken token, int col, int row, int delta)
+        {
+            foreach (var (x, y) in token.Points)
+            {
+                int absX = col + x;
+                int absY = row + y;
+                int idx = absY * Cols + absX;
+                tiles[idx] = (tiles[idx] + delta) % (Wrap + 1);
+            }
+        }
+
+        public bool AllClear() => tiles.All(val => val == 0);
+    }
+
+    public class ShapeToken
+    {
+        public List<(int X, int Y)> Points { get; }
+        public int Height { get; }
+        public int Width { get; }
+
+        public ShapeToken(string layout)
+        {
+            var rows = layout.Split(',');
+            Height = rows.Length;
+            Width = rows[0].Length;
+            Points = new List<(int X, int Y)>();
+
+            for (int y = 0; y < Height; y++)
+                for (int x = 0; x < Width; x++)
+                    if (rows[y][x] == '1')
+                        Points.Add((x, y));
+        }
+    }
+
+    public class ShapeShifterSolver
+    {
+        private TileGrid canvas;
+        private List<ShapeToken> tokens;
+        private readonly Dictionary<int, (int X, int Y)> placements = new Dictionary<int, (int X, int Y)>();
+
+        public ShapeShifterSolver(TileGrid canvas, List<ShapeToken> tokens)
+        {
+            this.canvas = canvas;
+            this.tokens = tokens;
+        }
+
+        public bool AttemptSolve(int idx = 0)
+        {
+            if (idx == tokens.Count)
+                return canvas.AllClear();
+
+            var token = tokens[idx];
+            for (int col = 0; col <= canvas.Cols - token.Width; col++)
+            {
+                for (int row = 0; row <= canvas.Rows - token.Height; row++)
+                {
+                    canvas.Mark(token, col, row, 1);
+
+                    if (AttemptSolve(idx + 1))
+                    {
+                        placements[idx] = (col, row);
+                        return true;
+                    }
+
+                    canvas.Mark(token, col, row, canvas.Wrap);
+                }
+            }
+
+            return false;
+        }
+
+        public Dictionary<int, (int X, int Y)> GetPlacements() => placements;
+    }
+}
